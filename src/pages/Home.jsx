e7f779c02f20cfbc02151dfd33b8e548b2865867
Braien Machado de Paula <braienmp@outlook.com>
2022-03-31 10:42:39 -0300

    style search input container

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,21 @@ import React from 'react';
 export default function Home() {
   return (
     <main>
-      <p>
-        Rest Countries API with Theme Switcher.
-      </p>
+      <label htmlFor="search-input" className="shadow-box" id="search-label">
+        <i className="fas fa-search" />
+        <input id="search-input" type="text" placeholder="Search for a country..." />
+      </label>
+      <label htmlFor="select-region" className="shadow-box">
+        Filter by Region:
+        <select name="region" id="select-region">
+          <option value="" selected>All</option>
+          <option value="Afria">Africa</option>
+          <option value="America">America</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europe</option>
+          <option value="Oceania">Oceania</option>
+        </select>
+      </label>
     </main>
   );
 }
