fafd6caf0931b713e9988026ebda9694992f6a3d
Braien Machado de Paula <braienmp@outlook.com>
2022-03-31 11:51:38 -0300

    style dropdown for dark theme

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,32 +4,42 @@ export default function Home() {
   const [region, setRegion] = useState('');
   const [isHidden, setIsHidden] = useState(true);
 
+  const toggleDropdown = () => {
+    setIsHidden(!isHidden);
+  };
+
   const dropdown = () => {
     const options = ['All', 'Africa', 'America', 'Asia', 'Europe', 'Oceania'];
     return (
-      <div className="shadow-box dropdown">
+      <div className="shadow-box box dropdown">
         {options.map((option) => (
-          <option value={option} onClick={() => setRegion(option)}>{option}</option>
+          <option
+            value={option}
+            onClick={() => {
+              setRegion(option);
+              toggleDropdown();
+            }}
+          >
+            {option}
+          </option>
         ))}
       </div>
     );
   };
 
-  const toggleDropdown = () => {
-    setIsHidden(!isHidden);
-  };
-
   return (
     <main>
-      <label htmlFor="search-input" className="shadow-box" id="search-label">
+      <label htmlFor="search-input" className="shadow-box box" id="search-label">
         <i className="fas fa-search" />
         <input id="search-input" type="text" placeholder="Search for a country..." />
       </label>
-      <button type="button" className="shadow-box" onClick={toggleDropdown}>
-        {`Filter by Region: ${region}`}
-        <i className="fas fa-chevron-circle-down" />
-      </button>
-      { isHidden ? null : dropdown() }
+      <div className="dropdown-container">
+        <button type="button" className="shadow-box box dropdown-button" onClick={toggleDropdown}>
+          {`Filter by Region: ${region}`}
+          <i className="fas fa-chevron-circle-down" />
+        </button>
+        { isHidden ? null : dropdown() }
+      </div>
     </main>
   );
 }
